66f8128d843faf3d4c1503e55ef486359f6b803e
Mark Jones <markjonestx@gmail.com>
2022-09-09 10:14:38 -0400

    Remove depreciated `register` keyword
    The `register` keyword is one of the carry-overs from C, but as of
    c++17 will throw errors in C++. Based off of my research though,
    removing the keyword should be fine since in C++ the keyword does
    not change any behaviour around variable declaration in C++

--- src/keyParse.cc
+++ src/keyParse.cc
@@ -372,7 +372,7 @@ union yyalloc
 #   define YYCOPY(To, From, Count)      \
       do                    \
     {                   \
-      register YYSIZE_T yyi;        \
+      YYSIZE_T yyi;        \
       for (yyi = 0; yyi < (Count); yyi++)   \
         (To)[yyi] = (From)[yyi];        \
     }                   \
@@ -540,7 +540,7 @@ yystrlen (yystr)
      const char *yystr;
 #   endif
 {
-  register const char *yys = yystr;
+  const char *yys = yystr;
 
   while (*yys++ != '\0')
     continue;
@@ -565,8 +565,8 @@ yystpcpy (yydest, yysrc)
      const char *yysrc;
 #   endif
 {
-  register char *yyd = yydest;
-  register const char *yys = yysrc;
+   char *yyd = yydest;
+   const char *yys = yysrc;
 
   while ((*yyd++ = *yys++) != '\0')
     continue;
@@ -650,8 +650,8 @@ YYPARSE_PARAM_DECL
     YY_DECL_VARIABLES
 #endif  /* !YYPURE */
 
-    register int yystate;
-    register int yyn;
+    int yystate;
+    int yyn;
     int yyresult;
     /* Number of tokens to shift before error messages enabled.  */
     int yyerrstatus;
@@ -669,12 +669,12 @@ YYPARSE_PARAM_DECL
     /* The state stack. */
     short yyssa[YYINITDEPTH];
     short *yyss = yyssa;
-    register short *yyssp;
+    short *yyssp;
 
     /* The semantic value stack.  */
     YYSTYPE yyvsa[YYINITDEPTH];
     YYSTYPE *yyvs = yyvsa;
-    register YYSTYPE *yyvsp;
+    YYSTYPE *yyvsp;
 
 #if YYLSP_NEEDED
     /* The location stack.  */
--- src/keyScan.cc
+++ src/keyScan.cc
@@ -603,9 +603,9 @@ YY_MALLOC_DECL
     YY_USER_ACTION
 
 YY_DECL {
-    register yy_state_type yy_current_state;
-    register char *yy_cp = NULL, *yy_bp = NULL;
-    register int yy_act;
+     yy_state_type yy_current_state;
+     char *yy_cp = NULL, *yy_bp = NULL;
+     int yy_act;
 
 #line 31 "keyScan.ll"
 
@@ -650,7 +650,7 @@ YY_DECL {
         yy_current_state = yy_start;
         yy_match:
         do {
-            register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
+            YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
             if (yy_accept[yy_current_state]) {
                 yy_last_accepting_state = yy_current_state;
                 yy_last_accepting_cpos = yy_cp;
@@ -920,9 +920,9 @@ YY_DECL {
  */
 
 static int yy_get_next_buffer() {
-    register char *dest = yy_current_buffer->yy_ch_buf;
-    register char *source = yytext_ptr;
-    register int number_to_move, i;
+     char *dest = yy_current_buffer->yy_ch_buf;
+     char *source = yytext_ptr;
+     int number_to_move, i;
     int ret_val;
 
     if (yy_c_buf_p > &yy_current_buffer->yy_ch_buf[yy_n_chars + 1])
@@ -1037,13 +1037,13 @@ static int yy_get_next_buffer() {
 /* yy_get_previous_state - get the state just before the EOB char was reached */
 
 static yy_state_type yy_get_previous_state() {
-    register yy_state_type yy_current_state;
-    register char *yy_cp;
+     yy_state_type yy_current_state;
+     char *yy_cp;
 
     yy_current_state = yy_start;
 
     for (yy_cp = yytext_ptr + YY_MORE_ADJ; yy_cp < yy_c_buf_p; ++yy_cp) {
-        register YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
+         YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
         if (yy_accept[yy_current_state]) {
             yy_last_accepting_state = yy_current_state;
             yy_last_accepting_cpos = yy_cp;
@@ -1075,10 +1075,10 @@ static yy_state_type yy_try_NUL_trans( yy_current_state )
 yy_state_type yy_current_state;
 #endif
 {
-    register int yy_is_jam;
-    register char *yy_cp = yy_c_buf_p;
+     int yy_is_jam;
+     char *yy_cp = yy_c_buf_p;
 
-    register YY_CHAR yy_c = 1;
+     YY_CHAR yy_c = 1;
     if (yy_accept[yy_current_state]) {
         yy_last_accepting_state = yy_current_state;
         yy_last_accepting_cpos = yy_cp;
@@ -1097,14 +1097,14 @@ yy_state_type yy_current_state;
 
 #ifndef YY_NO_UNPUT
 #ifdef YY_USE_PROTOS
-static void yyunput( int c, register char *yy_bp )
+static void yyunput( int c,  char *yy_bp )
 #else
 static void yyunput( c, yy_bp )
 int c;
-register char *yy_bp;
+ char *yy_bp;
 #endif
     {
-    register char *yy_cp = yy_c_buf_p;
+    char *yy_cp = yy_c_buf_p;
 
     /* undo effects of setting up yytext */
     *yy_cp = yy_hold_char;
@@ -1112,10 +1112,10 @@ register char *yy_bp;
     if ( yy_cp < yy_current_buffer->yy_ch_buf + 2 )
         { /* need to shift things up to make room */
         /* +2 for EOB chars. */
-        register int number_to_move = yy_n_chars + 2;
-        register char *dest = &yy_current_buffer->yy_ch_buf[
+         int number_to_move = yy_n_chars + 2;
+         char *dest = &yy_current_buffer->yy_ch_buf[
                     yy_current_buffer->yy_buf_size + 2];
-        register char *source =
+         char *source =
                 &yy_current_buffer->yy_ch_buf[number_to_move];
 
         while ( source > yy_current_buffer->yy_ch_buf )
@@ -1579,7 +1579,7 @@ yyconst char *s2;
 int n;
 #endif
     {
-    register int i;
+    int i;
     for ( i = 0; i < n; ++i )
         s1[i] = s2[i];
     }
@@ -1593,7 +1593,7 @@ static int yy_flex_strlen( s )
 yyconst char *s;
 #endif
     {
-    register int n;
+    int n;
     for ( n = 0; s[n]; ++n )
         ;
 
